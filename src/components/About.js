d280b72021245b2fc7a2dc6c41ecfff7961da27f
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-02-28 22:59:30 +0100

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ export default function About() {
             javascript en libraries zoals React.jen Redux-toolkit. In mijn vrije
             tijd houd ik er van om me zelf nog meer te ontwikkelen. ook vind ik
             het erg leuk om kleine projecten te doen voor hobby clubs die wel
-            een nieuwe React web site kunnen gebruiken. het liefst werk ik in
-            klein team verband zo kan je veel en snel van elkaar kan leren. ook vind
+            een nieuwe React web site kunnen gebruiken. het liefst werk ik in een
+            klein team verband zo kan je veel en snel van elkaar leren. ook vind
             ik het erg leuk om in nieuwen technieken te duiken zo als Vite en
             Next van react. daar naast volg ik ook nog vervolg opleiding bij
             freeCodeCamp. Ze hebben daar erg veel online opleidingen die mijn

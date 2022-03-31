a13b8e9df4d8401313c4a2926c7669005c0f90d9
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-03-31 22:23:40 +0200

    commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import wd0 from "../img/coffeetogo.png";
 import wd1 from "../img/Note.png";
 import wd2 from "../img/flower.png";
-import wd3 from "../img/wd3.jpg";
+import wd3 from "../img/Adidas.png";
 import wd4 from "../img/wd4.jpg";
 import wd5 from "../img/wd5.jpg";
 
@@ -9,14 +9,17 @@ import wd5 from "../img/wd5.jpg";
 
 
 export default function Projects() {
-    const proj1 = () => {
+    const proj0 = () => {
         window.open("https://github.com/Stefannieuwenburg/vite-single-page-website-react");
     };
-    const proj2 = () => {
+    const proj1 = () => {
         window.open("https://github.com/Stefannieuwenburg/vite-project-notes-react");
     };
-    const proj3 = () => {
+    const proj2 = () => {
         window.open("https://github.com/Stefannieuwenburg/vite-flower-project");
+    };
+    const proj3 = () => {
+        window.open("https://github.com/Stefannieuwenburg/vite-shoe-store-adidas");
     };
     const proj4 = () => {
         window.open("https://github.com/Stefannieuwenburg?tab=repositories");
@@ -24,40 +27,37 @@ export default function Projects() {
     const proj5 = () => {
         window.open("https://github.com/Stefannieuwenburg?tab=repositories");
     };
-    const proj6 = () => {
-        window.open("https://github.com/Stefannieuwenburg?tab=repositories");
-    };
     return (
         <div className="works">
             <div className="work-section">
                 <div className="cards">
                     <img src={wd0} alt="coffeetogo project" />
-                    <button onClick={proj1}>Code</button>
+                    <button onClick={proj0}>Code</button>
                 </div>
 
                 <div className="cards">
                     <img src={wd1} alt="img" />
-                    <button onClick={proj2}>Code</button>
+                    <button onClick={proj1}>Code</button>
                 </div>
 
                 <div className="cards">
                     <img src={wd2} alt="img" />
-                    <button onClick={proj3}>Code</button>
+                    <button onClick={proj2}>Code</button>
                 </div>
 
                 <div className="cards">
                     <img src={wd3} alt="img" />
-                    <button onClick={proj4}>Code</button>
+                    <button onClick={proj3}>Code</button>
                 </div>
 
                 <div className="cards">
                     <img src={wd4} alt="img" />
-                    <button onClick={proj5} >Code</button>
+                    <button onClick={proj4} >Code</button>
                 </div>
 
                 <div className="cards">
                     <img src={wd5} alt="img" />
-                    <button onClick={proj6} >Code</button>
+                    <button onClick={proj5} >Code</button>
                 </div>
             </div>
         </div>
